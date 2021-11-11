9a12fd831c49f2b6e00a3f40b960c7f804c5294d
NicoKeil <NicoKeil@github.com>
2021-11-11 19:08:13 -0300

    carrito de compras

---
+++ carts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from store.models import Product
+from .models import Cart, CartItem
+# Create your views here.
+def cart(request):
+	return render(request, "store/cart.html")
+
+def addCart(request, product_id):
+
+	product = Product.objects.get(id=product_id)
+
+	try:
+		cart = Cart.objects.get(cart_id=_cart_id(request))
+	except Cart.DoesNotExist:
+		cart = Cart.objects.create(
+			cart_id =  _cart_id(request)
+		)
+
+	cart.save()
+
+	try:
+		cart_item = CartItem.objects.get(product=product, cart=cart)
+		cart_item.quantity += 1 
+		cart_item.save()
+	except CartItem.DoesNotExist:
+		cart_item = CartItem.objects.create(
+			product = product,
+			quantity = 1,
+			cart = cart 
+		)
+		cart_item.save()
+
+	return redirect('cart')
+
+#Creat a new Session 
+def _cart_id(request):
+	cart = request.session.session_key
+
+	if not cart:
+		cart = request.session.create()
+
+	return cart
